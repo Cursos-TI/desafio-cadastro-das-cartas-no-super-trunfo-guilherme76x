1ef01358ab177f8d0e4af255663911f1f6e65137
guilherme da silva pressler lins <223234357+guilherme76x@users.noreply.github.com>
2025-08-02 01:29:34 +0000

    Atualizei meu codigo

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,43 +1,37 @@
 #include <stdio.h>
 
-int main(){
-    char Estado [1];
-    char Codigo [3];
-    char Cidade [20];
-    int Populaçao;
+int main() {
+    char Estado[20]; 
+    char Codigo[4];
+    char Cidade[20];
+    int Populacao; 
     int PontosTuristicos;
     float Area;
     float PIB;
 
     printf("Digite o nome do Estado: \n");
-    scanf("%s", &Estado);
-
+    scanf("%19s", Estado);
     printf("Digite o Codigo Da Carta: \n");
-    scanf("%s", &Codigo);
-
+    scanf("%3s", Codigo); 
     printf("Digite o Nome Da Cidade: \n");
-    scanf("%s", &Cidade);
-
-    printf("Digite o numero da Populaçao: \n");
-    scanf("%d", &Populaçao);
-
+    scanf("%19s", Cidade);
+    printf("Digite o numero da Populacao: \n");
+    scanf("%d", &Populacao);
     printf("Digite o numero de Pontos Turisticos: \n");
     scanf("%d", &PontosTuristicos);
-
     printf("Digite a Area em KM²: \n");
     scanf("%f", &Area);
-
     printf("Digite o PIB: \n");
     scanf("%f", &PIB);
 
-    printf("nome do Estado: %s", Estado);
-    printf("Codigo Da Carta: %s", Codigo);
-    printf("nome da Cidade: %s", Cidade);
-    printf("numero da Populaçao: %d", Populaçao);
-    printf("numero de Pontos Turisticos: %d", PontosTuristicos);
-    printf("a Area é: %f", Area");
-    printf("o PIB é: %f", PIB);
-
-    return 0;
+    printf("Nome do Estado: %s\n", Estado);
+    printf("Codigo Da Carta: %s\n", Codigo);
+    printf("Nome da Cidade: %s\n", Cidade);
+    printf("Numero da Populacao: %d\n", Populacao);
+    printf("Numero de Pontos Turisticos: %d\n", PontosTuristicos);
+    printf("A Area é: %.2f KM²\n", Area);
+    printf("O PIB é: %.2f\n", PIB); 
     
+    return 0;
+
 }
